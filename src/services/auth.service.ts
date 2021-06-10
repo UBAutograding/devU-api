f2916ce5f29896134ae8e03097be10238b57ae6f
David Dobmeier <daviddob@buffalo.edu>
2021-06-10 01:07:55 -0400

    [WIP] Implement additional JWT controls
    Adds additional JWT fields for validation as well as ensures that
    the signing algorithms and keys are validated. Additional changes
    and cleanup to come.
    
    ---WIP---
    Currently in a working state - however additional validation needs
    to be performed and lots of cleanup/restructure to follow.
    (Consider collapsing all the x.config.json files into a single yaml
    config file organized by domain with optional ENV var overrides for
    each?)

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,21 +1,38 @@
 import jwt from 'jsonwebtoken'
+import jws from 'jws'
+import fs from 'fs'
 
 import environment from '../environment'
 
 import { DeserializedToken, DeserializedRefreshToken } from 'devu-shared-modules'
 
-const accessOptions = { expiresIn: `${environment.tokenExpiration}s` }
-const refreshOptions = { expiresIn: '10d' }
+// TODO: Replace with proper app level configs
+const pathToEnv = `${__dirname}/../env/`
+const authConfig = JSON.parse(fs.readFileSync(pathToEnv + 'config/auth.config.json', 'utf8'))
+const signingKey = authConfig.jwt.keys[authConfig.jwt.active_key_id].private_key
+
+function getVerificationKey(kid: string) {
+  if (!authConfig.jwt.keys[kid]) {
+    return null
+  }
+  return authConfig.jwt.keys[kid].public_key
+}
+
+const commonOptions = { issuer: "devU-auth", audience: ["devU-api", "devU-client"], keyid: authConfig.jwt.active_key_id }
+const accessOptions = { ...commonOptions, expiresIn: `${environment.tokenExpiration}s` }
+// TODO: Add jwtid to refresh tokens for revocation ability in the future
+// or add rev_sig (hashed value of some fields that revoke tokens if any change)
+const refreshOptions = { ...commonOptions, expiresIn: '10d' }
 
 export function get(user: DeserializedToken) {
-  const token = jwt.sign(user, environment.jwtPrivateKey, accessOptions)
+  const token = jwt.sign({scope: ["profile", "cse250.read"]}, signingKey, {...accessOptions, subject: user.email, algorithm: "RS256" })
 
   return token
 }
 
 export function authenticate(token: string): DeserializedToken | null {
   try {
-    return jwt.verify(token, environment.jwtPrivateKey)
+    return jwt.verify(token, getVerificationKey(jws.decode(token).header.kid), {...accessOptions, algorithms: ["RS256"]})
   } catch (_err) {
     console.error(_err)
     return null
@@ -23,14 +40,14 @@ export function authenticate(token: string): DeserializedToken | null {
 }
 
 export function createRefresh(id: number) {
-  const token = jwt.sign({ id }, environment.jwtPrivateKey, refreshOptions)
+  const token = jwt.sign({ id }, signingKey, {...refreshOptions, algorithm: "RS256"})
 
   return token
 }
 
 export function authenticateRefresh(token: string): DeserializedRefreshToken | null {
   try {
-    return jwt.verify(token, environment.jwtPrivateKey)
+    return jwt.verify(token, getVerificationKey(jws.decode(token).header.kid), {...refreshOptions, algorithms: ["RS256"]})
   } catch (_err) {
     console.error(_err)
     return null
