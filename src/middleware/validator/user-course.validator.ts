cd7200eb0f26033d71e2c538b8e5cc590ce0c2eb
Jesse Hartloff <hartloff@buffalo.edu>
2021-06-22 16:27:25 -0400

    Add user-course serializer

--- src/middleware/validator/user-course.validator.ts
+++ src/middleware/validator/user-course.validator.ts
@@ -2,11 +2,11 @@ import { check } from 'express-validator'
 
 import validate from './generic.validator'
 
-const userId = check('course_id').isNumeric()
+const userId = check('user_id').isNumeric()
 const courseId = check('course_id').isNumeric()
 const level = check('level').isString().trim().isIn(['student', 'ta', 'instructor'])
 const lectureSection = check('lecture_section').isString().trim().isLength({max: 128}).optional({nullable: true})
-const dropped = check('dropped').isBoolean().optional({nullable: true})
+const dropped = check('dropped').isBoolean().optional()
 
 const validator = [userId, courseId, level, lectureSection, dropped, validate]
 
