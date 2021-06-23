338e48a1aabfd9d7ba64a0917c70187c81274363
David Dobmeier <daviddob@buffalo.edu>
2021-06-22 22:02:32 -0400

    Adding Authentication to API (#12)
    Implemented initial developer auth + SAML flows for authentication, uses JWT for tokens
    
    Co-authored-by: Michael Drewitt <mikedrewitt@gmail.com>

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -5,6 +5,9 @@ import { AccessToken, RefreshToken } from 'devu-shared-modules'
 declare global {
   namespace Express {
     interface Request {
+      // Auth Data
+      currentUser?: AccessToken // Deserialized access token
+      refreshUser?: RefreshToken // Deserialized refresh token
       user?: User
       users?: User[]
       userCourse?: UserCourse
