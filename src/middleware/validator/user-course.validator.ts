0508b61281eeef33ba214e1172c6287aa2b8df05
Jesse Hartloff <hartloff@buffalo.edu>
2021-07-05 13:39:17 -0400

    Update user-course validator to use the userCourseLevel array instead of string literals

--- src/middleware/validator/user-course.validator.ts
+++ src/middleware/validator/user-course.validator.ts
@@ -1,11 +1,13 @@
 import { check } from 'express-validator'
 
+import { userCourseRoles } from 'devu-shared-modules'
+
 import validate from './generic.validator'
 
 // TODO: check if user/course id exist
 const userId = check('userId').isNumeric()
 const courseId = check('courseId').isNumeric()
-const level = check('level').isString().trim().isIn(['student', 'ta', 'instructor'])
+const level = check('level').isString().trim().isIn(userCourseRoles.map(role => role.value))
 const lectureSection = check('lectureSection').isString().trim().isLength({ max: 128 }).optional({ nullable: true })
 const dropped = check('dropped').isBoolean().optional()
 
