d82d36029b950915df3c42728c73d0051528babf
Jesse Hartloff <hartloff@buffalo.edu>
2021-07-07 11:18:00 -0400

    Update PK anf FK constraint names to be human readable

--- src/migration/1625669170756-addUserCourseModel.ts
+++ src/migration/1625669170756-addUserCourseModel.ts
@@ -5,15 +5,15 @@ export class addUserCourseModel1625669170756 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "user_courses" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "course_id" integer NOT NULL, "level" character varying(128) NOT NULL, "lecture_section" character varying(128), "dropped" boolean NOT NULL DEFAULT false, "user_id" integer, CONSTRAINT "PK_0802c48ad82a16822bd7041154a" PRIMARY KEY ("id"))`
+      `CREATE TABLE "user_courses" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "course_id" integer NOT NULL, "level" character varying(128) NOT NULL, "lecture_section" character varying(128), "dropped" boolean NOT NULL DEFAULT false, "user_id" integer, CONSTRAINT "user_courses_primary_key_constraint" PRIMARY KEY ("id"))`
     )
     await queryRunner.query(
-      `ALTER TABLE "user_courses" ADD CONSTRAINT "FK_7ecb10d15b858768c36d37727f9" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+      `ALTER TABLE "user_courses" ADD CONSTRAINT "user_courses_to_users_foreign_key_constraint" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user_courses" DROP CONSTRAINT "FK_7ecb10d15b858768c36d37727f9"`)
+    await queryRunner.query(`ALTER TABLE "user_courses" DROP CONSTRAINT "user_courses_to_users_foreign_key_constraint"`)
     await queryRunner.query(`DROP TABLE "user_courses"`)
   }
 }
