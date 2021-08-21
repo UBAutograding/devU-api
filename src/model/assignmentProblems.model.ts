1fc87f8759e8d08f8272fe02e1d44d90fbbddfa9
Nicholas <32116122+nicholasmy@users.noreply.github.com>
2021-08-21 16:18:12 -0400

    Format

--- src/model/assignmentProblems.model.ts
+++ src/model/assignmentProblems.model.ts
@@ -1,38 +1,38 @@
 import {
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-    CreateDateColumn,
-    UpdateDateColumn,
-    DeleteDateColumn,
-    ManyToOne,
-    JoinColumn
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm'
 
 import AssignmentModel from './assignments.model'
 
 @Entity('assignment_problems')
 export default class AssignmentProblem {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @Column({name: 'assignment_id'})
-    @JoinColumn({name: 'assignment_id'})
-    @ManyToOne(() => AssignmentModel)
-    assignmentId: number
+  @Column({ name: 'assignment_id' })
+  @JoinColumn({ name: 'assignment_id' })
+  @ManyToOne(() => AssignmentModel)
+  assignmentId: number
 
-    @Column({name: 'problem_name', length: 128})
-    problemName: string
+  @Column({ name: 'problem_name', length: 128 })
+  problemName: string
 
-    @Column({name: 'max_score'})
-    maxScore: number
+  @Column({ name: 'max_score' })
+  maxScore: number
 
-    @CreateDateColumn({name: 'created_at'})
-    createdAt: Date
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date
 
-    @UpdateDateColumn({name: 'updated_at'})
-    updatedAt: Date
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date
 
-    @DeleteDateColumn({name: 'deleted_at'})
-    deletedAt?: Date
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt?: Date
 }
--- src/services/assignmentProblem.service.ts
+++ src/services/assignmentProblem.service.ts
@@ -1,37 +1,37 @@
-import {getRepository, IsNull} from 'typeorm'
+import { getRepository, IsNull } from 'typeorm'
 
 import AssignmentProblemModel from '../model/assignmentProblems.model'
 
-import {AssignmentProblem} from 'devu-shared-modules'
+import { AssignmentProblem } from 'devu-shared-modules'
 
 const connect = () => getRepository(AssignmentProblemModel)
 
 export async function create(assignmentProblem: AssignmentProblem) {
-    return await connect().save(assignmentProblem)
+  return await connect().save(assignmentProblem)
 }
 
 export async function update(assignmentProblem: AssignmentProblem) {
-    const {id, assignmentId, problemName, maxScore} = assignmentProblem
-    if (!id) throw new Error('Missing Id')
-    return await connect().update(id, {assignmentId, problemName, maxScore})
+  const { id, assignmentId, problemName, maxScore } = assignmentProblem
+  if (!id) throw new Error('Missing Id')
+  return await connect().update(id, { assignmentId, problemName, maxScore })
 }
 
 export async function _delete(id: number) {
-    return await connect().softDelete({id, deletedAt: IsNull()})
+  return await connect().softDelete({ id, deletedAt: IsNull() })
 }
 
 export async function retrieve(id: number) {
-    return await connect().findOne({id, deletedAt: IsNull()})
+  return await connect().findOne({ id, deletedAt: IsNull() })
 }
 
 export async function list() {
-    return await connect().find({deletedAt: IsNull()})
+  return await connect().find({ deletedAt: IsNull() })
 }
 
 export default {
-    create,
-    retrieve,
-    update,
-    _delete,
-    list,
+  create,
+  retrieve,
+  update,
+  _delete,
+  list,
 }
