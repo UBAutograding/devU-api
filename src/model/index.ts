efe6d950bb01752d71834c2557290b6eb16a9e73
John Abramo <jmabramo@buffalo.edu>
2022-10-05 17:06:05 -0400

    Added Submission Problem Score Model

--- src/model/index.ts
+++ src/model/index.ts
@@ -3,7 +3,8 @@ import CourseModel from './courses.model'
 import SubmissionModel from './submissions.model'
 import UserModel from './users.model'
 import UserCourseModel from './userCourses.model'
+import SubmissionProblemScoreModel from './submissionProblemScore.model'
 
-type Models = AssignmentModel | CourseModel | SubmissionModel | UserCourseModel | UserModel
+type Models = AssignmentModel | CourseModel | SubmissionModel | UserCourseModel | UserModel | SubmissionProblemScoreModel
 
 export default Models
